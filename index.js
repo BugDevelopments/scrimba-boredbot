d33c8930aeef8ec1c9fc1e05970dbff70e89fde9
Bugdeveloper <none@nowhere>
2025-07-30 01:02:50 +0200

    aria fixes

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/**
-Challenge: 
-
-- Make the styling more exciting once an activity idea comes 
-back from the Bored API
-    - Resources: DOM element "classList" property, uigradients.com, 
-      Google Fonts, color.adobe.com
-    - Some ideas:
-      - Change the title from "BoredBot" to something more exciting!
-      - Change the background to something less drab.
-      - Bonus: Animate something on the screen to move around and add more 
-        excitement to the page
-*/
-
-
 import { gifUrls } from "./data.js"
 
 console.log(gifUrls)
@@ -27,11 +12,9 @@ document.getElementById("get-activity").addEventListener("click", function() {
 
 
 function changeStyle() {
-  document.querySelector("h4").classList.add("h4-activated")
+  document.querySelector("p").classList.add("p-activated")
   const imgEl = document.getElementById("motivational-gif")
   const num = Math.floor(Math.random()*gifUrls.length)
   imgEl.src = gifUrls[num]
-  console.log(num)
-  console.log(imgEl.src)
   imgEl.style.display = "block"
 }
